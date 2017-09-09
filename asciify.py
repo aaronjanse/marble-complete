c343ebf9459795fb4fe52653fd2c524f09033ecd
aaronduino <aaron.janse@gmail.com>
2017-09-08 17:07:31 -0700

    implement asciify.py

---
+++ asciify.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+import fileinput
+
+transformations = {
+'╬': '+',
+'╗': '\\',
+'╔': '/',
+'╚': '\\',
+'╝': '/',
+'┼': '+',
+'╭': '.',
+'╮': '.',
+'╯': '.',
+'╰': '.',
+'┄': '.',
+'┆': '.',
+'↘': 't',
+'↙': 'T',
+'═': '-',
+'║': '|',
+'⇓': 'v',
+'⇑': '^',
+'⇒': '>',
+'⇐': '<',
+}
+
+original_code = ''.join(fileinput.input())
+transformed_code = ''
+for char in original_code:
+    if char in transformations:
+        transformed_code += transformations[char]
+    else:
+        transformed_code += char
+
+print(transformed_code)
